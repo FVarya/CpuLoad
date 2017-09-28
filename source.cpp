e6c29fd14dfce3d6f72578d5c235ccca51e78fc9
FVarya <fvarya@ya.ru>
2017-09-29 01:07:06 +0300

    almost all

--- source.cpp
+++ source.cpp
@@ -1,7 +1,10 @@
+
+
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include <chrono>
 #include <thread>
 #include <cstdlib>
@@ -45,7 +48,7 @@ private:
 			if (this->closeThread) {
 				break;
 			}
-			std::this_thread::sleep_for(microseconds(sleepTime));
+			std::this_thread::sleep_for(milliseconds(sleepTime));
 		}
 		return running_total;
 	}
@@ -89,25 +92,26 @@ private:
 		std::vector<double> X;
 
 		void inverse() {
-			double determinant
-			for (i = 0; i < 3; i++)
-				determinant = determinant + (A[0][i] * (A[1][(i + 1) % 3] * A[2][(i + 2) % 3] - 
+			double determinant;
+			for (int i = 0; i < 3; i++)
+				determinant += (A[0][i] * (A[1][(i + 1) % 3] * A[2][(i + 2) % 3] - 
 					A[1][(i + 2) % 3] * A[2][(i + 1) % 3]));
 
+			std::cout << determinant << std::endl;
 			std::vector<std::vector<double>> mat;
-			for (i = 0; i < 3; i++) {
+			mat.resize(3);
+			for (int i = 0; i < 3; i++) {
 				mat[i].resize(3);
-				for (j = 0; j < 3; j++) {
-					mat[i].resize(3);
-					mat[i] = ((A[(j + 1) % 3][(i + 1) % 3] * A[(j + 2) % 3][(i + 2) % 3]) -
-						(A[(j + 1) % 3][(i + 2) % 3] * A[(j + 2) % 3][(i + 1) % 3]));
+				for (int j = 0; j < 3; j++) {
+					mat[i][j] = ((A[(j + 1) % 3][(i + 1) % 3] * A[(j + 2) % 3][(i + 2) % 3]) -
+						(A[(j + 1) % 3][(i + 2) % 3] * A[(j + 2) % 3][(i + 1) % 3]))/ determinant;
 				}
 			}
 
 			A = mat;
 		}
 	public:
-		SysOfLinearEquation(std::vector<double> x, std::vector<double> y) {
+		SysOfLinearEquation(std::vector<double> x, std::vector<int> y) {
 			A.resize(x.size());
 			B.resize(x.size());
 			X.resize(x.size());
@@ -115,9 +119,12 @@ private:
 			for (int i = 0; i < x.size(); i++) {
 				A[i].resize(x.size(), 1);
 				B[i] = y[i];
+				std::cout << A[i][0] << " ";
 				for (int j = 1; j < x.size(); j++) {
 					A[i][j] = pow(x[i], j);
+					//std::cout << A[i][j] << " ";
 				}
+				//std::cout << std::endl;
 			}
 		}
 
@@ -127,8 +134,11 @@ private:
 				double sumInLine = 0;
 				for (int j = 0; j < 3; j++) {
 					sumInLine += A[i][j] * B[j];
+					//std::cout << A[i][j] << "  ";
+
 				}
-				X[i] = sumInLine
+				X[i] = sumInLine;
+				std::cout << X[i] <<  std::endl;
 			}
 			return X;
 		}
@@ -148,32 +158,28 @@ public:
 		CPU_ZERO(&my_set);
 		CPU_SET(0, &my_set);
 		sched_setaffinity(0, sizeof(cpu_set_t), &my_set);*/
-		std::vector<int> sleepTime { 10000, 80000, 300000 };
+		std::vector<int> sleepTime { 2, 20, 300};
 		std::vector<double> cpuUsage(3);
 		
-		for(auto it = sleepTime.begin(); it != myvector.end(); ++it){
+		for(int i = 0; i < sleepTime.size(); i++){
 			memory d = getCurrentLoad();
-			startLoad(*it);
-			std::this_thread::sleep_for(microseconds((*it)*15));
+			startLoad(sleepTime[i]);
+			std::this_thread::sleep_for(milliseconds(1500));
 			memory l = getCurrentLoad();
 			stopLoad();	
-			cpuUsage.push_back(100.0*(l.busy - d.busy) / (l.work - d.work));
-			std::cout<< 100.0*(l.busy - d.busy) / (l.work - d.work) << " " << sleepTime[i] << std::endl;
+			cpuUsage[i] = 100.0*(l.busy - d.busy) / (l.work - d.work);
+			std::cout << cpuUsage[i] << " " << sleepTime[i] << std::endl;
+
 		}
 
-		SysOfLinearEquation sys(sleepTime, cpuUsage);
-		this->coef = sys.solve();
-		std::cout << "cpuUsage " << this->coeff[0] + this->coeff[1] * load + this->coeff[2] * pow(load, 2) << std::endl;
+		SysOfLinearEquation sys(cpuUsage, sleepTime);
+		this->coeff = sys.solve();
+		std::cout << "cpuUsage " << this->coeff[0] + this->coeff[1] * 70 + this->coeff[2] * pow(70, 2) << std::endl;
 	}
 
 
 	void setLoad(double load, int time) {
-		for (int i = 0; i < 4; i++) {
-			if (load >= this->coef[i].interval[0] && load <= this->coef[i].interval[1]) {
-				this->thr = std::thread(&LoadGenerator::generateLoad, this, this->coeff[0] + this->coeff[1]*load + this->coeff[2]*pow(load,2));
-				break;
-			}
-		}
+		startLoad(this->coeff[0] + this->coeff[1] * load + this->coeff[2] * pow(load, 2));
 	}
 };
 
